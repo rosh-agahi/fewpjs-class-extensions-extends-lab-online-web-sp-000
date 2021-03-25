f719c563ec5a88e9def33f98ffb71c3fe021bc4a
Roshanak Agahi <rosh.agahi@gmail.com>
2021-03-25 20:46:46 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Your code here
+class Polygon {
+  constructor(sideLengths) {
+  this.sideLengths = []
+  }
+  
+  get countSides(){
+    return this.sideLengths.length()
+  }
+  
+  get perimeter() {
+    return this.sideLengths.reduce
+  }
+}
